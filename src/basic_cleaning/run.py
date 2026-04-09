b1a55ec17ae28ec5ecf99a150e5d580ebe3e8f10
Student <student@example.com>
2026-04-09 12:43:26 +0000

    Final fix with geo-filtering and duplicates

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,12 +28,17 @@ def go(args):
     df = pd.read_csv(artifact_local_path)
 
     # 2- Filter outliers using the provided arguments
+    df = df.drop_duplicates()
+    df = df.dropna(subset=["price"])
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
-    # 3- Convert date to datetime (as we did in the notebook)
+    # 3- Convert date to datetime 
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    #3.1 add boundary filter
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     # 4- Save the cleaned DataFrame locally
     df.to_csv("clean_sample.csv", index=False)
 
